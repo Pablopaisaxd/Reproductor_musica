7691836210ee43914fa7caa1af0b904f09496fc2
Pablopaisaxd <pablogira71@gmail.com>
2025-09-11 01:00:54 -0500

    subida de archivos

--- src/components/File.jsx
+++ src/components/File.jsx
@@ -1,39 +1,74 @@
 import React, { useState } from 'react'
+import canciones from  "/src/data/canciones.json"
+import imgdefault from "/src/assets/portadas/unown.jpg"
 
 export const File = () => {
-  const [file, setFile] = useState(null)
+  const [files, setFiles] = useState([])
   const [error, setError] = useState("")
 
   const handleFileChange = (e) => {
-    const selectedFile = e.target.files[0]
-    if (!selectedFile) return
+    const selectedFiles = Array.from(e.target.files)
 
-    
     const allowedTypes = ["audio/mpeg", "audio/wav", "audio/ogg", "audio/mp4"]
-    if (allowedTypes.includes(selectedFile.type)) {
-      setFile(selectedFile)
+    const validFiles = selectedFiles.filter(file =>
+      allowedTypes.includes(file.type)
+    )
+
+    if (validFiles.length > 0) {
+      setFiles(validFiles)
       setError("")
+
+      const coincidencias = validFiles.map((file) => {
+        const nombreSinExtension = file.name.replace(/\.[^/.]+$/, "");
+        const cancionEncontrada = canciones.find(
+          (c) => c.titulo.toLowerCase() === nombreSinExtension.toLowerCase()
+        );
+
+        return {
+          archivo: file.name,
+          titulo: nombreSinExtension,
+          artista: cancionEncontrada ? cancionEncontrada.artista : "Desconocido",
+          duracion: cancionEncontrada ? cancionEncontrada.duracion : "N/A",
+          peso: (file.size / (1024 * 1024)).toFixed(2) + " MB",
+          imagen: cancionEncontrada ? cancionEncontrada.imagen : imgdefault
+        };
+      });
+
+      setFiles(coincidencias);
     } else {
-      setFile(null)
+      setFiles([])
       setError("Solo se permiten archivos de música (.mp3, .wav, .ogg, .m4a)")
     }
   }
 
+  const artistaCancion = canciones.map(cancion => {
+    console.log(cancion)
+  })
+
+  
+  console.log(files)
   return (
     <div>
       <input
         type="file"
+        multiple
         onChange={handleFileChange}
         accept=".mp3,.wav,.ogg,.m4a" 
       />
-      
+
       {error && <p style={{ color: 'red' }}>{error}</p>}
-      {file && (
-        <div>
-          <p>Nombre: {file.name}</p>
-          <p>Tipo: {file.type}</p>
-          <p>Tamaño: {Math.round(file.size / 1024 / 1024)} MB</p>
-        </div>
+
+      {files.length > 0 && (
+        <ul>
+          {files.map((file, index) => (
+            <li key={index}>
+              <p>Titulo: {file.titulo}</p>
+              <p>Artista: {file.artista}</p>
+              <p>Peso: {file.peso}</p>
+              <img src={file.imagen} alt="" />
+            </li>
+          ))}
+        </ul>
       )}
     </div>
   )
